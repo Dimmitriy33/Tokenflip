e26e033776274b3c6b40e2e8345af393f38fe996
Dmitry <schalunov002@gmail.com>
2023-10-08 22:07:50 +0300

    Revert "UI. Vitalik loh 12"
    This reverts commit a463a91fa75dbbe210622b38fcf49dc790f5f356.

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -33,6 +33,8 @@ export default function HomePage() {
   const [curHash, setCurHash] = useState<string | null>(null);
   const [users, setUsers] = useState<Array<any>>([]);
 
+  const [fullUsers, setFullUsers] = useState<Array<any>>([]);
+
   const [prevGame, setPrevGame] = useState<IPrevGame | null>(null);
   const [resAnim, serResAnim] = useState<boolean>(false);
   const [showRes, setShowRes] = useState<boolean>(false);
@@ -68,6 +70,7 @@ export default function HomePage() {
       const next = data.next;
       setTime(next.timestamp + 60 - Math.floor(Date.now() / 1000));
       setUsers([]);
+      setFullUsers([]);
       getGameUsersF();
       setCurHash(next.md5);
       setBetPlaced(false);
@@ -135,18 +138,19 @@ export default function HomePage() {
     const res = await getGameUsers();
     //@ts-ignore
     const data = await res.json();
+    setFullUsers(data);
 
-    addUser(0, data);
+    addUser(0);
   }, []);
 
-  const addUser = (time: number, data: Array<any>) => {
+  const addUser = (time: number) => {
     setTimeout(() => {
       const uL = users.length;
-      const user = data[uL];
+      const user = fullUsers[uL];
       if (user && users.findIndex((v) => v.id === user.id) === -1) {
         setUsers([...users, user]);
-        if (uL + 1 < data.length) {
-          addUser(time + Math.floor(Math.random() * (2000 - 200 + 1) + 200), data);
+        if (uL + 1 < fullUsers.length) {
+          addUser(time + Math.floor(Math.random() * (2000 - 200 + 1) + 200));
         }
       }
     }, time);
